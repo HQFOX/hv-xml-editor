30bcefab611dc1a7c703586b22368050bee74e62
hqfox <henrique.raposo95@gmail.com>
2024-07-24 14:57:00 +0100

    fix: dependecies

--- vite.config.ts
+++ vite.config.ts
@@ -11,43 +11,46 @@ import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
 import { HvAppShellVitePlugin } from "@hitachivantara/app-shell-vite-plugin";
 
 export default defineConfig(({ mode }) => ({
-  plugins: [
-    react({
-      jsxImportSource: "@emotion/react",
-      babel: {
-        plugins: ["@emotion/babel-plugin"],
-      },
-    }),
-    tsconfigPaths(),
-    unoCSS({ mode: "per-module" }),
-    cssInjectedByJsPlugin({
-      relativeCSSInjection: true,
-    }),
-    HvAppShellVitePlugin({
-      mode,
-      autoViewsAndRoutes: true,
-      autoMenu: true
-    }),
-  ],
+	optimizeDeps: {
+		exclude: ["xmllint-wasm"],
+	},
+	plugins: [
+		react({
+			jsxImportSource: "@emotion/react",
+			babel: {
+				plugins: ["@emotion/babel-plugin"],
+			},
+		}),
+		tsconfigPaths(),
+		unoCSS({ mode: "per-module" }),
+		cssInjectedByJsPlugin({
+			relativeCSSInjection: true,
+		}),
+		HvAppShellVitePlugin({
+			mode,
+			autoViewsAndRoutes: true,
+			autoMenu: true,
+		}),
+	],
 
-  test: {
-    globals: true,
-    environment: "happy-dom",
-    setupFiles: ["src/tests/setupTests.ts"],
-    reporters: "default",
-    coverage: {
-      enabled: false, // disabled by default. run vitest with --coverage
-      provider: "v8",
-      reporter: "lcov",
-      include: ["src/**/*.ts?(x)"],
-      exclude: [
-        "src/**/mocks/*",
-        "src/**/tests/*",
-        "src/**/*.test.ts?(x)",
-        "src/**/styles.[jt]s?(x)",
-        "src/**/*.d.ts",
-        "src/*.tsx",
-      ],
-    },
-  },
+	test: {
+		globals: true,
+		environment: "happy-dom",
+		setupFiles: ["src/tests/setupTests.ts"],
+		reporters: "default",
+		coverage: {
+			enabled: false, // disabled by default. run vitest with --coverage
+			provider: "v8",
+			reporter: "lcov",
+			include: ["src/**/*.ts?(x)"],
+			exclude: [
+				"src/**/mocks/*",
+				"src/**/tests/*",
+				"src/**/*.test.ts?(x)",
+				"src/**/styles.[jt]s?(x)",
+				"src/**/*.d.ts",
+				"src/*.tsx",
+			],
+		},
+	},
 }));
