e49094d8b6126c8294703ea2dc3cba812fde526f
hqfox <henrique.raposo95@gmail.com>
2024-07-24 12:21:12 +0100

    chore: project setup

---
+++ README.md
@@ -0,0 +1,10 @@
+# hv-xml-editor
+
+## How to use
+
+Install it and run:
+
+```sh
+npm install
+npm run dev
+```
---
+++ app-shell.config.ts
@@ -0,0 +1,12 @@
+import type { HvAppShellConfig } from "@hitachivantara/app-shell-vite-plugin";
+
+export default (): HvAppShellConfig => ({
+  name: "hv-xml-editor",
+
+  baseUrl: "/",
+
+
+  mainPanel: {
+    maxWidth: "xl"  },
+
+});
